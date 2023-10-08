1f5b303e4a87cb75058244bd005bda6bf91786c7
AsifCDRY <147215511+AsifCDRY@users.noreply.github.com>
2023-10-07 22:15:28 -0400

    Update FoodCategory.java

--- ShoppingList/src/coe528lab4/FoodCategory.java
+++ ShoppingList/src/coe528lab4/FoodCategory.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Asif Choudhury, 501111719
+ * @author Asif Choudhury
  */
 public class FoodCategory extends FoodComponent {
     private String category;
